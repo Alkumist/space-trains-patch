b6162ae194a9617c9b7b4099d672a32b33b80500
Alkumist <ponyyouman@gmail.com>
2025-06-23 00:02:25 -0600

    initial upload to Github

---
+++ data-updates.lua
@@ -0,0 +1,91 @@
+data.raw["locomotive"]["locomotive"].surface_conditions = {
+      {
+        property = "gravity",
+        min = 0,
+        max = 1000
+      }
+    }
+data.raw["cargo-wagon"]["cargo-wagon"].surface_conditions = {
+      {
+        property = "gravity",
+        min = 0,
+        max = 1000
+      }
+    }
+data.raw["fluid-wagon"]["fluid-wagon"].surface_conditions = {
+      {
+        property = "gravity",
+        min = 0,
+        max = 1000
+      }
+    }
+data.raw["rail-signal"]["rail-signal"].surface_conditions = {
+      {
+        property = "gravity",
+        min = 0,
+        max = 1000
+      }
+    }
+data.raw["rail-chain-signal"]["rail-chain-signal"].surface_conditions = {
+      {
+        property = "gravity",
+        min = 0,
+        max = 1000
+      }
+    }
+data.raw["curved-rail-b"]["curved-rail-b"].surface_conditions = {
+      {
+        property = "gravity",
+        min = 0,
+        max = 1000
+      }
+    }
+data.raw["curved-rail-a"]["curved-rail-a"].surface_conditions = {
+      {
+        property = "gravity",
+        min = 0,
+        max = 1000
+      }
+    }
+data.raw["half-diagonal-rail"]["half-diagonal-rail"].surface_conditions = {
+      {
+        property = "gravity",
+        min = 0,
+        max = 1000
+      }
+    }
+data.raw["straight-rail"]["straight-rail"].surface_conditions = {
+      {
+        property = "gravity",
+        min = 0,
+        max = 1000
+      }
+    }
+data.raw["train-stop"]["train-stop"].surface_conditions = {
+      {
+        property = "gravity",
+        min = 0,
+        max = 1000
+      }
+    }
+data.raw["item"]["rail-signal"].surface_conditions = {
+      {
+        property = "gravity",
+        min = 0,
+        max = 1000
+      }
+    }
+data.raw["item"]["rail-chain-signal"].surface_conditions = {
+      {
+        property = "gravity",
+        min = 0,
+        max = 1000
+      }
+    }
+
+--data.raw["rail-ramp"]["rail-ramp"].surface_conditions = one_gravity_condition
+--data.raw["elevated-straight-rail"]["elevated-straight-rail"].surface_conditions = one_gravity_condition
+--data.raw["elevated-half-diagonal-rail"]["elevated-half-diagonal-rail"].surface_conditions = one_gravity_condition
+--data.raw["elevated-curved-rail-a"]["elevated-curved-rail-a"].surface_conditions = one_gravity_condition
+--data.raw["elevated-curved-rail-b"]["elevated-curved-rail-b"].surface_conditions = one_gravity_condition
+--data.raw["rail-support"]["rail-support"].surface_conditions = one_gravity_condition
